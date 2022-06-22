81fa3ea97cb88d7b164e26dd88136a0236a05585
nsalei <natashokich67@gmail.com>
2022-06-22 08:33:35 +0400

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+let result = 0;
+let divideResult = 0;
+n = Math.abs(n);
+    while (n > 0) {
+        divideResult = n % 10;
+        result = result * 10 + divideResult;
+        n = Math.trunc(n / 10);
+    }
+    return result;
 }
